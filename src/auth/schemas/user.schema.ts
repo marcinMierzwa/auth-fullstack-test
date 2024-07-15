a4a8c3d5bb9c6dbbf2fa7da1869ff83bba3fe4e2
Marcin Mierzwa <marcin.mierzwa81@gmail.com>
2024-07-15 23:04:17 +0200

    first commit

---
+++ src/auth/schemas/user.schema.ts
@@ -0,0 +1,21 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+@Schema({
+  timestamps: true,
+  versionKey: false,
+})
+export class User extends Document {
+
+  @Prop({ unique: true, required: true })
+  name: string;
+
+  @Prop({ unique: true, required: true })
+  email: string;
+
+  @Prop({required: true })
+  password: string;
+
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
